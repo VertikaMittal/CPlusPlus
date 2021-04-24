264d751c6f878ec83a64c2ee84924b664910069b
Vertika <mittalvertika1803200@gmail.com>
2021-04-24 22:28:31 +0530

    destructor prog

---
+++ destr.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class introduce{
+    private:
+    string name;
+    public:
+    introduce()
+    {
+        name="noname";
+    }
+    ~introduce()
+    {
+        cout<<"destructor called";
+    }
+    introduce(string iname)
+    {
+        name=iname;
+    }
+    void setname(string value)
+    {
+        name=value;
+    }
+    void myname()
+    {
+        cout<<"my name is "<<name<<endl;
+    }
+};
+int main(){
+    // introduce obj("vertika mittal");
+    // obj.setname("Vertika mittal");
+    // obj.myname();
+    
+
+    introduce *obj2=new introduce("noni mittal");
+    // obj2->name="Noni";
+    obj2->myname();
+    delete obj2;
+    return 0;
+}
