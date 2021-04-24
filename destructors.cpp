708255f9deb3471c8a004f8eda5028a67e1e716e
Vertika <mittalvertika1803200@gmail.com>
2021-04-24 22:17:53 +0530

    destructor prog

--- destructors.cpp
+++ destructors.cpp
@@ -9,6 +9,10 @@ class introduce{
     {
         name="noname";
     }
+    ~introduce()
+    {
+        cout<<"destructor called";
+    }
     introduce(string iname)
     {
         name=iname;
@@ -23,12 +27,14 @@ class introduce{
     }
 };
 int main(){
-    introduce obj("vertika mittal");
-    // obj.setname("Vertika mittal");
-    obj.myname();
+    // introduce obj("vertika mittal");
+    // // obj.setname("Vertika mittal");
+    // obj.myname();
+    
 
     introduce *obj2=new introduce("noni mittal");
     // obj2->name="Noni";
     obj2->myname();
+    delete obj2;
     return 0;
 }
